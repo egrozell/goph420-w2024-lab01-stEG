cbb4db6996a2a88eff84ebedf7e5e49a64c64a45
egrozell <ethan.grozell@ucalgary.ca>
2024-03-03 21:32:50 -0700

    implementiation of trapazoid rule

--- src/goph420_lab01/integration.py
+++ src/goph420_lab01/integration.py
@@ -1,3 +1,4 @@
+import numpy as np 
 def integrate_newton(x,f,alg):
     """
     inputs 
@@ -7,14 +8,29 @@ def integrate_newton(x,f,alg):
     alg: string trap or simp to indicate the use of trapazoid rule or Simpson's rule  
     outputs
     _______
-    estamate: float
+    s: float
 
     raises
     ______
     ValueError: if alg is not simp or trap
     ValueError: if x and f are to many dimensions or different lengths
-    TypeError: if str-like 
+    ValueError: if x is not in acending order
+    TypeError: if alg is str-like 
     """
+    if alg != 'simp' | 'trap':
+        raise ValueError(f"alg {alg} is not simp or trap")
+    if np.shape(x) != np.shape(f):
+        raise ValueError(f"x and f have different lengths or shape {np.shape(f)} != {np.shape(x)}")
+    n = len(x)
+    for i in range (n-1):
+        if x[i+1] < x[i]: raise ValueError("x arrary not in acending order")
+    if alg == 'trap':
+        s = np.zeros((n))
+        for k in range(n-1):
+            s[k] = s[k-1] + (x[k]-x[k-1])*(f[k]+f[k-1])/2
+
+    return s
+
 
 def integrate_gauss(f,lims,npts:int=3):
     """
