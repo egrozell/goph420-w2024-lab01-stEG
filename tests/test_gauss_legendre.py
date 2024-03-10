1c9ca5b999622680a1cfad546e17c5053c3e6ba2
egrozell <ethan.grozell@ucalgary.ca>
2024-03-10 16:00:16 -0600

    unit testing complete

--- tests/test_gauss_legendre.py
+++ tests/test_gauss_legendre.py
@@ -1,13 +1,72 @@
 import unittest
 
 from goph420_lab01.integration import (integrate_gauss)
-def function(x):
-    return (2-x)
-class TestGauss(unittest.TestCase):
+class TestGauss_npts_1(unittest.TestCase):
     def setUp(self):
         self.lims = [0,2]
+        self.f = lambda x: x-2
     def test_value_ntps1(self):
         expected = 2
-        self.assertAlmostEqual(integrate_gauss(function(),self.lims,1),expected,delta=1e-15)
+        self.assertAlmostEqual(integrate_gauss(self.f,self.lims,1),expected,delta=1e-15)
     def test_type(self):
-        self.assertIsInstance(integrate_gauss(function(),self.lims,1), float)
+        self.assertIsInstance(integrate_gauss(self.f,self.lims,1), float)
+
+class TestGauss_npts_2(unittest.TestCase):
+    def setUp(self):
+        self.lims = [0,2]
+        self.f = lambda x: 3x**2 
+    def test_value_ntps1(self):
+        expected = 8
+        self.assertAlmostEqual(integrate_gauss(self.f,self.lims,2),expected,delta=1e-15)
+    def test_type(self):
+        self.assertIsInstance(integrate_gauss(self.f,self.lims,2), float)
+
+class TestGauss_npts_3(unittest.TestCase):
+    def setUp(self):
+        self.lims = [0,2]
+        self.f = lambda x: 4x**3
+    def test_value_ntps1(self):
+        expected = 16
+        self.assertAlmostEqual(integrate_gauss(self.f,self.lims,3),expected,delta=1e-15)
+    def test_type(self):
+        self.assertIsInstance(integrate_gauss(self.f,self.lims,3), float)
+
+class TestGauss_npts_4(unittest.TestCase):
+    def setUp(self):
+        self.lims = [0,2]
+        self.f = lambda x: 5x**4
+    def test_value_ntps1(self):
+        expected = 32
+        self.assertAlmostEqual(integrate_gauss(self.f,self.lims,4),expected,delta=1e-15)
+    def test_type(self):
+        self.assertIsInstance(integrate_gauss(self.f,self.lims,4), float)
+
+class TestGauss_npts_5(unittest.TestCase):
+    def setUp(self):
+        self.lims = [0,2]
+        self.f = lambda x: 6x**5
+    def test_value_ntps1(self):
+        expected = 64
+        self.assertAlmostEqual(integrate_gauss(self.f,self.lims,5),expected,delta=1e-15)
+    def test_type(self):
+        self.assertIsInstance(integrate_gauss(self.f,self.lims,5), float)
+
+class TestGaussInvalidInitializers(unittest.TestCase):
+    
+    def test_callable_function(self):
+        with self.assertRaises(TypeError):
+            integrate_gauss(f:=8.0, lims = [1,2])
+
+    def test_lims_dim(self):
+        with self.assertRaises(ValueError):
+            integrate_gauss(f lambda x: 2-x, lims = [1])
+
+    def test_lims_type_lims(self):
+        with self.assertRaises(ValueError):
+            integrate_gauss(f lambda x: 2-x, lims = ['one','four'])
+
+    def test_npts(self):
+        with self.assertRaises(ValueError):
+            integrate_gauss(f lambda x: 2-x, lims = [1,4],npts = 0)
+if __name__ == "__main__":
+    unittest.main()
--- tests/test_newton_cotes.py
+++ tests/test_newton_cotes.py
@@ -1,22 +1,60 @@
 import unittest
 
 from goph420_lab01.integration import (integrate_newton)
-class TestGauss(unittest.TestCase):
+class test_trap_even(unittest.TestCase):
     def setUp(self):
-        self.x_trap= [0,1,2]
-        self.f_trap= [2,1,0]
-        self.x_simp_even = [0,1,2,3,4,5]
-        self.x_simp_even = [0,3,12,27,48,75]
-        self.x_simp_odd = [0,1,2,3,4]
-        self.x_simp_odd = [0,3,12,27,48]
+        self.x_trap= [0,2]
+        self.f_trap= [2,0]
     def test_value_trap(self):
         expected = 2
         self.assertEqual(integrate_newton(self.x_trap,self.f_trap,'trap'),expected)
+    def test_type(self):
+        self.assertIsInstance(integrate_newton(self.x_trap,self.f_trap,'trap'),float)
+
+class test_trap_odd(unittest.TestCase):
+    def setUp(self):
+        self.x_trap= [0,1,2]
+        self.f_trap= [2,1,0]
+    def test_value_trap(self):
+        expected = 2
+        self.assertEqual(integrate_newton(self.x_trap,self.f_trap,'trap'),expected)
+    def test_type(self):
+        self.assertIsInstance(integrate_newton(self.x_trap,self.f_trap,'trap'),float)
+
+class test_simp_even(unittest.TestCase):
+    def setUp(self):
+        self.x_simp_even = [0,1,2,3,4,5]
+        self.x_simp_even = [0,3,12,27,48,75]
     def test_value_simp_even(self):
         expected = 125
         self.assertEqual(integrate_newton(self.x_simp_even,self.f_simp_even,'simp'),expected)
+
+class test_simp_odd(unittest.TestCase):
+    def setUp(self):
+        self.x_simp_odd = [0,1,2,3,4]
+        self.x_simp_odd = [0,3,12,27,48]
+
     def test_value_simp_odd(self):
         expected = 64
         self.assertAlmostEqual(integrate_newton(self.x_simp_odd,self.f_simp_odd,'simp'),expected,delta=1e-15)
-    def test_type(self):
-        self.assertIsInstance(integrate_newton(self.x_trap,self.f_trap,'trap'),float)
+
+class TestGaussInvalidInitializers(unittest.TestCase):
+    def setUp(self):
+        self.x= [0,1,2]
+        self.xs= [1,2]
+        self.xm= [[1,2],[0,3]]
+        self.f= [2,1,0]
+    
+    def test_string_method(self):
+        with self.assertRaises(ValueError):
+            integrate_newton(integrate_newton(self.x,self.f,'hi'))
+
+    def test_dims(self):
+        with self.assertRaises(ValueError):
+            integrate_newton(integrate_newton(self.xm,self.f,'trap'))
+
+    def test_length(self):
+        with self.assertRaises(ValueError):
+            integrate_newton(integrate_newton(self.xs,self.f,'trap'))
+if __name__ == "__main__":
+    unittest.main()
